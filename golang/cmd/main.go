d30d3a6d7657d13a1963fd11d6df71b3d4dfa429
Sebastian Matias Segura <segurasebastian7887@gmail.com>
2022-01-04 16:19:06 -0300

    Add Golang interaction with Smart Contract

--- golang/cmd/main.go
+++ golang/cmd/main.go
@@ -1,37 +1,78 @@
 package main
 
 import (
+	"context"
 	"crypto/ecdsa"
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"golang.org/x/crypto/sha3"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
+	"math/big"
+	"seb7887/goeth/internal/store"
 )
 
-const ADDRESS = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
-
 func main() {
-	privateKey, err := crypto.GenerateKey()
+	client, err := ethclient.Dial("http://localhost:8545")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	privKeyBytes := crypto.FromECDSA(privateKey)
-	log.Println(hexutil.Encode(privKeyBytes)[2:])
-
-	publicKey := privateKey.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	privateKey, err := crypto.HexToECDSA("ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
-	log.Println(hexutil.Encode(publicKeyBytes)[4:])
+	publicKey := privateKey.Public()
+	publicKeyEDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal(err)
+	}
 
-	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
-	log.Println(address)
+	fromAddress := crypto.PubkeyToAddress(*publicKeyEDSA)
+	log.Println(fromAddress)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	hash := sha3.NewLegacyKeccak256()
-	hash.Write(publicKeyBytes[1:])
-	log.Println(hexutil.Encode(hash.Sum(nil)[12:]))
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0) // in Wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	address := common.HexToAddress("0x5FbDB2315678afecb367f032d93F642f64180aa3")
+	instance, err := store.NewStore(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var (
+		key [32]byte
+		value [32]byte
+	)
+
+	copy(key[:], []byte("foo"))
+	copy(value[:], []byte("bar"))
+
+	tx, err := instance.SetItem(auth, key, value)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("tx sent")
+	log.Println(tx.Hash().Hex())
+
+	res, err := instance.Items(nil, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(string(res[:]))
 }
